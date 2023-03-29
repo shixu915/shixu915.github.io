d4ddac0f48c69b5aa275f1e0bb41d6aa76943a11
azuimi <58015322+shixu915@users.noreply.github.com>
2023-03-29 17:35:16 +0800

    Update cau.py

--- cau.py
+++ cau.py
@@ -1,13 +1,12 @@
-import re
-import ast
 from bridge.context import ContextType
 from bridge.reply import Reply, ReplyType
 import plugins
 from plugins import *
 from common.log import logger
+import re
+import sympy
 
-
-@plugins.register(name="Calculator", desc="A simple calculator plugin", version="0.1", author="lanvent", desire_priority= -1)
+@plugins.register(name="Calculator", desc="A plugin that calculates mathematical expressions", version="0.1", author="YourName", desire_priority= 10)
 class Calculator(Plugin):
     def __init__(self):
         super().__init__()
@@ -15,29 +14,33 @@ class Calculator(Plugin):
         logger.info("[Calculator] inited")
 
     def on_handle_context(self, e_context: EventContext):
-
         if e_context['context'].type != ContextType.TEXT:
             return
-        
-        content = e_context['context'].content
-        logger.debug("[Calculator] on_handle_context. content: %s" % content)
 
-        if "计算" in content:
-            expression = re.sub(r'[^\d+\-*/\s().]', '', content.replace("计算", "")).strip()
-            try:
-                result = ast.literal_eval(expression)
-                reply = Reply()
-                reply.type = ReplyType.TEXT
-                reply.content = f"计算结果为：{result}"
-                e_context['reply'] = reply
-                e_context.action = EventAction.BREAK_PASS # 事件结束，并跳过处理context的默认逻辑
-            except:
-                reply = Reply()
-                reply.type = ReplyType.TEXT
-                reply.content = "无法计算，请检查表达式是否正确。"
-                e_context['reply'] = reply
-                e_context.action = EventAction.BREAK_PASS # 事件结束，并跳过处理context的默认逻辑
+        content = e_context['context'].content
+
+        if content.startswith("计算"):
+            expression = content[2:].strip()
+            reply = Reply()
+            reply.type = ReplyType.TEXT
+
+            if not expression:
+                reply.content = "请输入一个数学表达式。"
+            else:
+                try:
+                    # 使用sympy库解析并计算表达式
+                    result = sympy.sympify(expression)
+                    reply.content = f"计算结果：{result}"
+                except (sympy.SympifyError, ValueError) as e:
+                    logger.error(f"计算出错：{e}")
+                    reply.content = "无法计算，请检查您输入的表达式是否正确。"
+                except Exception as e:
+                    logger.error(f"未知错误：{e}")
+                    reply.content = "发生了一个未知错误，请稍后再试。"
+
+            e_context['reply'] = reply
+            e_context.action = EventAction.BREAK_PASS
 
     def get_help_text(self, **kwargs):
-        help_text = "发送包含“计算”关键词的消息，我会尝试计算表达式的结果。\n"
+        help_text = "输入“计算 数学表达式”，我会帮您计算数学表达式的结果。\n"
         return help_text
